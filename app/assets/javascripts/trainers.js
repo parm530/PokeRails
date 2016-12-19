59aefb4690b5ca7cb1fdb07a1c9d1ce83ab30b2d
parm530 <parmsahadeo530@gmail.com>
2016-12-19 17:08:36 -0500

    final bootstrap touches

--- app/assets/javascripts/trainers.js
+++ app/assets/javascripts/trainers.js
@@ -2,11 +2,12 @@ $(function() {
   $("#all-trainers").on('click', function(event) {
     event.preventDefault();
     $.get('/users.json', function(data) {
-      var string = "<h1>Trainers:</h1>";
+      var string = '<div class="container"><div class="row"><div class="col-lg-4"><h1>Trainers:</h1>';
       $.each(data, function(index, value) {
         var name = value.name;
-        string += "<li>" + name + "</li><br>";
+        string += "<h4><li>" + name + "</li></h4>";
       });
+      string += "</div></div></div>"
         $("#trainer").append(string);
     });
   });
